9b0776cec34a35d77ae46a0106c1653d513ab1ad
seipan1213 <seiju1213@gmail.com>
2022-06-25 20:31:24 +0900

    libft norm 修正

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,15 +6,15 @@
 /*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 20:47:14 by sehattor          #+#    #+#             */
-/*   Updated: 2022/06/19 19:39:13 by sehattor         ###   ########.fr       */
+/*   Updated: 2022/06/25 20:31:02 by sehattor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void put_num(long n, int fd)
+static void	put_num(long n, int fd)
 {
-	char c;
+	char	c;
 
 	if (n > 9)
 		ft_putnbr_fd(n / 10, fd);
@@ -22,9 +22,9 @@ static void put_num(long n, int fd)
 	write(fd, &c, 1);
 }
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	long num;
+	long	num;
 
 	num = n;
 	if (num < 0)
