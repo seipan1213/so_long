3925e9fccf0ce6f3c9fa6998fb2fabbcc1a6fb11
seipan1213 <seiju1213@gmail.com>
2022-07-18 10:34:57 +0900

    バグ修正

--- srcs/map_check_1.c
+++ srcs/map_check_1.c
@@ -6,25 +6,25 @@
 /*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:52:09 by sehattor          #+#    #+#             */
-/*   Updated: 2022/06/27 21:57:19 by sehattor         ###   ########.fr       */
+/*   Updated: 2022/07/18 10:34:23 by sehattor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	check_map(t_so_long *sl)
+void check_map(t_so_long *sl)
 {
 	check_map_char(sl);
 	is_close_map(sl);
 }
 
-void	check_map_char(t_so_long *sl)
+void check_map_char(t_so_long *sl)
 {
-	int	x;
-	int	y;
-	int	pl;
-	int	goal;
-	int	item;
+	int x;
+	int y;
+	int pl;
+	int goal;
+	int item;
 
 	pl = 0;
 	goal = 0;
@@ -47,16 +47,15 @@ void	check_map_char(t_so_long *sl)
 		put_exit_err(ERR_MAP_MISSING);
 }
 
-void	bfs_utils(t_so_long *sl, int **map, t_list **lst, int *item_g_sum)
+void bfs_utils(t_so_long *sl, int **map, t_list **lst, int *item_g_sum)
 {
-	const int	dx[4] = {0, 1, 0, -1};
-	const int	dy[4] = {1, 0, -1, 0};
-	t_ipair		p;
-	int			c;
+	const int dx[4] = {0, 1, 0, -1};
+	const int dy[4] = {1, 0, -1, 0};
+	t_ipair p;
+	int c;
 
 	p = *(t_ipair *)(*lst)->content;
-	if (sl->gm.map[p.first][p.second] == ITEM
-		|| sl->gm.map[p.first][p.second] == GOAL)
+	if (sl->gm.map[p.first][p.second] == ITEM || sl->gm.map[p.first][p.second] == GOAL)
 		(*item_g_sum)++;
 	ft_lstdel_front(lst);
 	c = -1;
@@ -64,21 +63,19 @@ void	bfs_utils(t_so_long *sl, int **map, t_list **lst, int *item_g_sum)
 		put_exit_err(ERR_MAP_NOT_ENCLOSED);
 	while (++c < 4)
 	{
-		if (sl->gm.map[p.first + dx[c]][p.second + dy[c]] != WALL
-			&& sl->gm.map[p.first + dx[c]][p.second + dy[c]] != GOAL
-			&& map[p.first + dx[c]][p.second + dy[c]] == 0)
+		if (sl->gm.map[p.first + dx[c]][p.second + dy[c]] != WALL && sl->gm.map[p.first + dx[c]][p.second + dy[c]] != GOAL && map[p.first + dx[c]][p.second + dy[c]] == 0)
 		{
 			ft_lstadd_back(lst,
-				ft_lstnew(make_ipair(p.first + dx[c], p.second + dy[c])));
+						   ft_lstnew(make_ipair(p.first + dx[c], p.second + dy[c])));
 			map[p.first + dx[c]][p.second + dy[c]] = 1;
 		}
 	}
 }
 
-void	map_bfs(t_so_long *sl, int **map, t_ipair p)
+void map_bfs(t_so_long *sl, int **map, t_ipair p)
 {
-	t_list	**lst;
-	int		item_g_sum;
+	t_list **lst;
+	int item_g_sum;
 
 	lst = (t_list **)ft_calloc(1, sizeof(t_list *));
 	*lst = ft_lstnew(make_ipair(p.first, p.second));
@@ -86,15 +83,15 @@ void	map_bfs(t_so_long *sl, int **map, t_ipair p)
 	while (*lst)
 		bfs_utils(sl, map, lst, &item_g_sum);
 	bfs_clear(lst);
-	if (item_g_sum != sl->gm.item_sum + 1)
+	if (item_g_sum != sl->gm.item_sum)
 		put_exit_err(ERR_MAP_CANT_FINISH);
 }
 
-void	is_close_map(t_so_long *sl)
+void is_close_map(t_so_long *sl)
 {
-	int		**map;
-	int		i;
-	t_ipair	p;
+	int **map;
+	int i;
+	t_ipair p;
 
 	i = 0;
 	map = (int **)malloc(sizeof(int *) * sl->gm.height);
